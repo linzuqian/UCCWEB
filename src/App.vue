863475b297b4444ef05329400e5a0bb28346f5ec
linzuqian <364698133@qq.com>
2018-04-24 15:30:44 +0800

    新增底部
    新增底部

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <ucc-header></ucc-header>
     <router-view></router-view>
+    <ucc-footer></ucc-footer>
   </div>
 </template>
 
 <script>
 import uccHeader from '@/components/header/item.vue'
+import uccFooter from '@/components/footer/item.vue'
 
 export default {
   name: 'App',
   components: {
-    uccHeader
+    uccHeader,
+    uccFooter
   }
 }
 </script>
